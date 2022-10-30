ed292e8b0ec927859b1be45eb692d7abe41807bc
Chandrababu Gowda <96734849+chandrababu-gowda@users.noreply.github.com>
2022-10-30 20:40:01 +0530

    Display the forecast

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let locationSearch = document.querySelector(".enter-location");
 let submit = document.querySelector(".submit-btn");
+let displayTemp = document.querySelector(".temp");
+let displayDesc = document.querySelector(".desc");
+let displayImg = document.querySelector(".icon");
 
 const options = {
   method: "GET",
@@ -22,7 +25,15 @@ submit.addEventListener("click", () => {
         .then((response) => response.json())
         .catch((err) => console.error(err))
         .then((response) => {
+          let temperature = response[0].Temperature.Metric.Value;
+          let unit = response[0].Temperature.Metric.Unit;
+          let description = response[0].WeatherText;
+          let icon = response[0].WeatherIcon;
+          displayTemp.innerHTML = `${temperature} °${unit}`;
+          displayDesc.innerHTML = `${description}`;
+          displayImg.src = `./images/${icon}.png`;
           console.log(response);
+          console.log(temperature, unit, description);
         });
     });
 });
