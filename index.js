191e26650ec81c10c9ff30356cc71837de2319d3
Chandrababu Gowda <96734849+chandrababu-gowda@users.noreply.github.com>
2022-10-30 19:41:16 +0530

    Create the UI

---
+++ index.js
@@ -0,0 +1,21 @@
+// const options = {
+//     method: "GET",
+//   };
+
+// fetch(
+//     `https://imdb8.p.rapidapi.com/title/get-ratings?tconst=${movieId}`,
+//     options
+//   )
+//     .then((response) => response.json())
+//     .then(response => console.log(response))
+//     .catch((err) => console.error(err));
+
+//  Location Key ---- http://dataservice.accuweather.com/locations/v1/search?q=Delhi&apikey=KFN3QrWNJCPTSSwZ8TGjWPraDWLInSFV;
+
+// apikey  =  KFN3QrWNJCPTSSwZ8TGjWPraDWLInSFV
+// Delhi Lat = 28.643  Lon = 77.118
+// Location Key = 202396
+
+//  Current forecast ---- http://dataservice.accuweather.com/currentconditions/v1/202396?apikey=KFN3QrWNJCPTSSwZ8TGjWPraDWLInSFV
+
+//  5 days forecast ---- http://dataservice.accuweather.com/forecasts/v1/daily/5day/202396?apikey=KFN3QrWNJCPTSSwZ8TGjWPraDWLInSFV
